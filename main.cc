31eb5fe4929512fe6332ae4836a4083c6344f16a
John Vishnefske <john.vishnefske@viavisolutions.com>
2022-10-11 13:24:28 -0500

    web browser

--- main.cc
+++ main.cc
@@ -1,19 +1,36 @@
 #include <memory>
-#include <iostream>
 #include <webkit2/webkit2.h>
 #include <gtk/gtk.h>
+#include <unistd.h>
 
 extern "C"{
-void activate(GtkApplication * app, gpointer user_data){
+void activate(GtkApplication *app, gpointer user_data)
+{
     (void) user_data;
-  GtkWidget * window;
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Window");
-  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-
-  gtk_widget_show_all (window);
+    const auto char_url = getenv("KIOSK_URL");
+    std::string url;
+    if(char_url == nullptr){
+        url = "https://example.com/";
+    }else{
+        url = char_url;
+    }
+//    std::shared_ptr<WebKitWebView> p_web_view{WEBKIT_WEB_VIEW(webkit_web_view_new()), g_object_unref};
+    WebKitWebView *web_view = WEBKIT_WEB_VIEW(webkit_web_view_new());
+//    std::shared_ptr<WebKitSettings> p_settings{webkit_settings_new(), g_object_unref};
+    WebKitSettings *settings = webkit_settings_new();
+    webkit_settings_set_enable_javascript(settings, true);
+    webkit_settings_set_enable_java(settings, false);
+    webkit_settings_set_enable_caret_browsing(settings, false);
+    webkit_settings_set_enable_developer_extras(settings, true);
+    webkit_settings_set_enable_fullscreen(settings, true);
+    webkit_web_view_set_settings(web_view, settings);
+    webkit_web_view_load_uri(web_view, url.c_str());
+    GtkWindow *window{GTK_WINDOW(gtk_application_window_new(app))};
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_container_add(GTK_CONTAINER(window), GTK_WIDGET(web_view));
+    gtk_widget_show_all(GTK_WIDGET(window));
 }
-} // extern
+}
 
 int main(int argc, char** argv){
     std::shared_ptr<GtkApplication> p_app{gtk_application_new(nullptr, G_APPLICATION_FLAGS_NONE), g_object_unref};
